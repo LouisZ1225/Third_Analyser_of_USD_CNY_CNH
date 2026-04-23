02c80bb9e5e44d2a33ed06e7085b3720b6e49bbd
LouisZ1225 <zhanggz1225@gmail.com>
2026-04-23 23:28:24 +0800

    upgrade for the plotly

--- Project/Mathplot/mathplot.py
+++ Project/Mathplot/mathplot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def mathplot(df):
 
@@ -24,20 +25,60 @@ def plotly_basic(df):
     fig.show()
 
 def plotly_upgrade(df):
-    fig = go.Figure()
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
+
+#====== Spread =======
 
     fig.add_trace(go.Scatter(
             x=df.index,
             y=df["spread"],
             mode="lines",
             name="Spread",
-            line=dict(width=1)
-    ))
+            line=dict(width=1, color="cyan"),
+            opacity=0.4),
+            row=1,
+            col=1
+    )
+
+#====== MA =======
+
+    fig.add_trace(go.Scatter(
+            x=df.index,
+            y=df["ma_20"],
+            mode="lines",
+            name="MA(20)",
+            line=dict(width=2, color="orange"),
+            opacity=0.8),
+            row=1,
+            col=1
+    )
+
+#====== volatility ======
+
+    fig.add_trace(go.Scatter(
+            x=df.index,
+            y=df["volatility"],
+            mode="lines",
+            name="Volatility",
+            line=dict(width=1, color="purple")),
+            row=2,
+            col=1
+    )
+
+#====== 0轴 ======= 
 
     fig.add_hline(
         y=0,
         line_dash="dash",
         line_color="gray"
+    )
+    
+    fig.update_layout(
+        title="CNH - CNY Spread Analysis | Mean Reversion Signal Monitor",
+        xaxis_title="Date",
+        yaxis_title="Spread",
+        template="plotly_dark",
+        hovermode="x unified"
     )
 
     fig.show()
