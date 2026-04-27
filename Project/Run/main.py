af5c7b23a42ed91d43b275d1bb059e0d43a33b27
LouisZ1225 <zhanggz1225@gmail.com>
2026-04-27 22:58:31 +0800

    update of ols model

--- Project/Run/main.py
+++ Project/Run/main.py
@@ -3,26 +3,25 @@ from Calculate.calculate_spread import calc_spread
 from Calculate.calculate_volatility import calc_volatility
 from Mathplot.mathplot import mathplot, plotly_basic,plotly_upgrade
 from Database.query import query_rate
+from Model.ols_model import run_ols
 
 DB_PATH = "Database/fx_rate.db"
 
 def main():
     print("🚀 开始运行 pipeline...")
 
-    # 1️⃣ 读取数据
     df = query_rate(DB_PATH)
     print("✅ 数据读取完成")
 
-    # 2️⃣ 计算指标
     df = calc_spread(df)
     df = calc_MA(df, 20)
     df = calc_volatility(df, 20)
-
+    
+    model = run_ols(df)
+    
     print("✅ 指标计算完成")
 
-    # 3️⃣ 画图
     plotly_upgrade(df)
-
     print("🎯 Pipeline 完成")
 
 if __name__ == "__main__":
